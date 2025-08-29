23c3f1029860996c1659c401ea5c1d1e14ecf31f
ResNeedle <kemmachat.muangkaew@gmail.com>
2025-08-29 17:13:38 +0700

    [Add] implement depth-based stamina reduction to the Cup class
    also in this commit:
    - Cup component now require 2 transforms
      to mark where the cup's top and bottom is.
         - for finding depth of the cookie relative to top and bottom.
    - Add ExtensionMethod
         - for the Remap() Methof

--- Assets/Scripts/Dip Areas/Cup.cs
+++ Assets/Scripts/Dip Areas/Cup.cs
@@ -2,17 +2,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Cup : DipArea
-{ 
+{
+
+    [SerializeField] Transform _cupTopPoint, _cupBottomPoint;
+    [Space]
+    [SerializeField] AnimationCurve _staminaReductionCurve;
+    [Space]
     [SerializeField] DipArea[] containedDipAreas;
+
+    float _cookieDepthf;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        
+    }
 
     private void FixedUpdate()
     {
+        TrackCookieDepth();
+
         ReduceCookieStamina();
     }
 
     protected override void OnEnterEffect()
     {
-        
+
     }
 
     protected override void OnStayEffect()
@@ -25,9 +44,28 @@ public class Cup : DipArea
         
     }
 
+    void TrackCookieDepth()
+    {
+        if (_cookie)
+        {
+            var cookie_y = _cookie.transform.position.y;
+
+            var top_y = _cupTopPoint.position.y;
+            var bottom_y = _cupBottomPoint.position.y;
+
+            // map top and bottom to 0.0 - 1.0
+            // get where cookie_y sit at between 0.0 - 1.0
+            _cookieDepthf = ExtensionMethod.Remap(cookie_y, top_y, bottom_y, 0, 1);
+
+
+        }
+    }
+
     void ReduceCookieStamina()
     {
-        _cookie?.ReduceStamina(1);
+        var relative_depth = _staminaReductionCurve.Evaluate(_cookieDepthf);
+
+        _cookie?.ReduceStamina(relative_depth);
     }
 }
 
